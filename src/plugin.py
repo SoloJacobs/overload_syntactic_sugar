a5289cb676f4253b9145f6c41a5cac23d5e168b3
Solomon Jacobs <solomonjacobs@protonmail.com>
2022-08-02 14:35:09 +0200

    plugin: addition of get_base_class_hook
    Invoking our plugin every time GetItem is used in a type definition
    is unwanted. Instead we want to change the definition of certain
    classes, which inherit from GetItem. For this purpose, we add
    `get_base_class_hook`. From running `test_plugin.py` we see this
    gives the desired result.

--- src/plugin.py
+++ src/plugin.py
@@ -3,9 +3,13 @@ from mypy.plugin import Plugin
 
 class CustomPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str) -> None:
-        # see explanation below
         if fullname.startswith("sol.GetItem"):
-            print(fullname)
+            print(f"get_type_analyze_hook({fullname})")
+
+    def get_base_class_hook(self, fullname: str) -> None:
+        if fullname.startswith("sol.GetItem"):
+            print(f"get_base_class_hook({fullname})")
+
 
 
 def plugin(version: str) -> type[CustomPlugin]:
--- src/target.py
+++ src/target.py
@@ -2,10 +2,14 @@ from sol import GetItem
 
 # reveal_type(GetItem())
 
+# Unwanted syntax
+
 _: GetItem
 
 def f(_: GetItem) -> None:
     ...
+
+# Wanted syntax
 
 class Test(GetItem):
     a: int
